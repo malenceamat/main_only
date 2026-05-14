3b5d5a0b566d91a6395da436cbc82dabcfcb1b96
malenceamat <yatikivan@gmail.com>
2026-05-14 05:54:09 +0300

    8 task

--- www/bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ www/bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001778724431';
-$dateexpire = '001778728031';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1778726050;}';
+$datecreate = '001778726856';
+$dateexpire = '001778730456';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1778727881;}';
 return true;
 ?>
